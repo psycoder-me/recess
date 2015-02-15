234023c49ca44e8e8b684f13c07949b15d8bfdca
Sourabh Verma <sourabh.coder@gmail.com>
2015-02-16 03:25:06 +0530

    Rename project to recess

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 #!/usr/bin/env node
 var fs = require('fs');
-var precessr = require('./precessr')
+var recess = require('./recess')
 
 var source = process.argv.splice(2)[0];
 var target = source.substring(0, source.lastIndexOf('.')) + '.css';
@@ -8,9 +8,9 @@ var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 fs.readFile(source, 'utf-8', function (error, data) {
     if (error) throw error;
 
-    data = precessr.analyse(data.split('\n'))
-    data = precessr.parse(data);
-    precessr.printParseTree(data);
+    data = recess.analyse(data.split('\n'))
+    data = recess.parse(data);
+    recess.printParseTree(data);
 
     fs.writeFile(target, data, function (error){
         if (error) throw error;
