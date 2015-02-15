0d7c4be16a30432ec35e3741b88353625d918e4b
Sourabh Verma <sourabh.coder@gmail.com>
2015-02-16 03:22:25 +0530

    Improve Analyser

--- index.js
+++ index.js
@@ -8,9 +8,9 @@ var target = source.substring(0, source.lastIndexOf('.')) + '.css';
 fs.readFile(source, 'utf-8', function (error, data) {
     if (error) throw error;
 
-    data = precessr.split(data.split('\n'))
-    data = precessr.treefy(data);
-    precessr.printTree(data);
+    data = precessr.analyse(data.split('\n'))
+    data = precessr.parse(data);
+    precessr.printParseTree(data);
 
     fs.writeFile(target, data, function (error){
         if (error) throw error;
--- precessr.js
+++ precessr.js
@@ -1,18 +1,29 @@
-var split = function (lines) {
+var analyse = function (lines) {
     return lines.map(function (line) {
         var part = line.trimLeft();
+        var selectors = ':.#>*+~[';
+        if (selectors.indexOf(part[0]) !== -1) {
+            var left = part.trim(),
+                right = '',
+                isPseudo = true;
+        } else {
+            var left = (part.split(':')[0] || '').trim()
+                right = (part.split(':')[1] || '').trim(),
+                isPseudo = false;
+        }
 
         return {
             indent: line.replace(part, '').length,
-            name: part,
+            left: left,
+            right: right,
             isPseudo: part[0] === ':'
         };
     }).filter(function (element) {
-        return element.name;
+        return element.left || element.right;
     });
 };
 
-var treefy = function (lines) {
+var parse = function (lines) {
     var parents = [];
 
     return lines.map(function (line) {
@@ -44,14 +55,19 @@ function indent (i) {
     return Array(i).join(' ');
 }
 
-var printTree = function (tree, i) {
+var printParseTree = function (tree, i) {
     i = i || 0;
     tree.forEach(function (e) {
-        console.log(indent(e.indent) + e.name);
-        printTree(e.children, i + 1);
+        console.log(indent(e.indent) + e.left + (e.right ? ': ' : '') + e.right);
+        printParseTree(e.children, i + 1);
     });
 }
 
-exports.split = split;
-exports.treefy = treefy;
-exports.printTree = printTree;
+var compile = function (tree) {
+
+};
+
+exports.analyse = analyse;
+exports.parse = parse;
+exports.printParseTree = printParseTree;
+exports.compile = compile;
