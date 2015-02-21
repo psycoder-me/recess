a75f860ac68ad85b8de94cf28c56b65f0e9637fa
Sourabh Verma <sourabh.coder@gmail.com>
2015-02-22 02:32:05 +0530

    Update project structure and add Package.json

--- index.js
+++ index.js
@@ -1,18 +1,1 @@
-#!/usr/bin/env node
-var fs = require('fs');
-var recess = require('./recess')
-
-var source = process.argv.splice(2)[0];
-var target = source.substring(0, source.lastIndexOf('.')) + '.css';
-
-fs.readFile(source, 'utf-8', function (error, data) {
-    if (error) throw error;
-
-    data = recess.analyse(data.split('\n'))
-    data = recess.parse(data);
-    recess.printParseTree(data);
-
-    fs.writeFile(target, data, function (error){
-        if (error) throw error;
-    });
-});
+module.exports = require('./lib/recess');
